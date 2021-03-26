89739ba824d0f2c13a7a783f490105af82c12dfc
lcx <864255598@qq.com>
2021-03-26 20:50:38 +0800

    ''

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,149 @@
 <template>
-  <div>
-    <h1>首页</h1>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+
+  <el-container class="home-container">
+      <!-- 头部区域 -->
+  <el-header>
+      <div class="headbox">
+          <img src="../assets/heima.png" alt="">
+          <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+  </el-header>
+  <el-container>
+      <!-- 侧边栏 -->
+    <el-aside :width="isCollapse?'64px' : '200px'" ref="aside">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+      background-color="#333744"
+      text-color="#fff"
+      unique-opened
+      :collapse="isCollapse"
+      :collapse-transition="false"
+      router
+      :default-active="activePath"
+      active-text-color="#409EFF">
+      
+       <!-- 一级菜单 -->
+      <el-submenu :index="index+''" v-for="(item,index) in menuList" :key="item.id">
+           <!-- 一级菜单的模板 -->
+        <template slot="title">
+            <!-- 图标 -->
+          <i :class="iconsObj[index]"></i>
+          <!-- 文本 -->
+          <span>{{item.authName}}</span>
+        </template>
+        <!-- 二级菜单 -->
+         <el-menu-item :index="'/'+i.path+''" v-for="i in item.children" :key="i.id" @click="changeActive('/'+i.path+'')">
+                 <!-- 一级菜单的模板 -->
+        <template slot="title">
+            <!-- 图标 -->
+          <i class="el-icon-menu"></i>
+          <!-- 文本 -->
+          <span>{{i.authName}}</span>
+        </template>
+         </el-menu-item>
+      </el-submenu>
+     
+    
+    </el-menu>
+    </el-aside>
+    <!-- 右侧 主体区域 -->
+    <el-main><router-view></router-view></el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
 export default {
+   data(){
+       return {
+        //    左侧菜单数据
+           menuList:[],
+           iconsObj:{
+               '0':"iconfont icon-user",
+               '1':"iconfont icon-tijikongjian",
+               '2':"iconfont icon-shangpin",
+               '3':"iconfont icon-danju",
+               '4':"iconfont icon-baobiao"
+           },
+           menuIndex:'',
+        //    是否折叠
+           isCollapse:false,
+        //    被激活的连接地址  
+           activePath:'/users'
+       }
+
+   },
   methods: {
+    //   保存连接的激活状态
+      changeActive(activePath){
+          sessionStorage.setItem('activePath',activePath)
+        this.activePath = activePath
+      },
     logout() {
       sessionStorage.clear()
       this.$router.push("/login")
     },
+   async getMeunList(){
+      const {data:res} = await this.$http.get('menus');
+      if(res.meta.status!==200) return this.$message.error(res.meta.msg);
+      this.menuList = res.data
+      console.log(res);
+    },
+    // 点击按钮切换菜单的折叠与展开
+    toggleCollapse(){
+        this.isCollapse = !this.isCollapse
+    }
   },
+   created(){
+       this.activePath = window.sessionStorage.getItem('activePath')
+        this.getMeunList()
+    }
 }
 </script>
 
 <style lang='less' scoped>
+.el-header {
+    display: flex;
+    justify-content: space-between;
+    background-color: #373d41;
+    padding-left: 0;
+    align-items: center;
+}
+.el-aside {
+    background-color: #333744;
+}
+.el-main {
+    background-color: #eaedf1;
+}
+.home-container {
+    height: 100%;
+}
+.headbox {
+    display: flex;
+    color: #fff;
+    font-size: 20px;
+    margin-bottom: 10px;
+    align-items: center;
+   span{
+       margin-left: 15px;
+    }
+}
+.iconfont {
+    margin-right: 10px;
+}
+.el-menu {
+    border-right: 0px;
+}
+.toggle-button {
+    text-align: center;
+    line-height: 24px;
+    height: 24px;
+    color: #fff;
+    background-color: #4A5064;
+    cursor: pointer;
+    letter-spacing: 3px;
+    font-size: 10px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import {Message,Input,Form,FormItem,Button} from "element-ui"
+import {Message,Input,Form,FormItem,Button,Header,Main,Aside,Container,
+  Menu,Submenu,MenuItem,MenuItemGroup} from "element-ui"
 import './assets/css/global.css'
 import'element-ui/lib/theme-chalk/index.css';
 import './assets/fonts/iconfont.css'
@@ -12,11 +13,27 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 // 配置请求的根路径
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.interceptors.request.use(config=>{
+  console.log(config);
+  // 最后必须return config
+  console.log(sessionStorage.getItem('token'));
+  config.headers.Authorization = sessionStorage.getItem('token');
+  return config
+})
 Vue.prototype.$http = axios
  Vue.use(Form)
  Vue.use(FormItem)
  Vue.use(Input)
  Vue.use(Button)
+ Vue.use(Header)
+ Vue.use(Main)
+ Vue.use(Aside)
+ Vue.use(Container)
+ Vue.use(Menu)
+ Vue.use(Submenu)
+ Vue.use(MenuItem)
+ Vue.use(MenuItemGroup)
+
  Vue.prototype.$message = Message
 new Vue({
   el: '#app',
